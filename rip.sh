41b9cf9ad1be2ab5dc94a29408288bd202a35ae6
Cliffss <82544006+Cliffss@users.noreply.github.com>
2021-06-11 20:26:31 +0100

    Update rip.sh

--- rip.sh
+++ rip.sh
@@ -5,8 +5,8 @@ randomize() { grep '[^ \t]' | sort -R; }
 echo 'Enter your desired amount of lines generated'
 read lines
 
-a=/home/cliffs/rip/words/adjectives
-n=/home/cliffs/rip/words/nouns
+a=~/rip/words/adjectives
+n=~/rip/words/nouns
 
 while (( lines-- )) && read a && read n <&3
 do
