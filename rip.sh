52987de91f675a1fea9c13c1a24f5ba7c6584382
Cliffs <82544006+Cliffss@users.noreply.github.com>
2021-11-25 18:02:34 +0000

    Update rip.sh

--- rip.sh
+++ rip.sh
@@ -5,11 +5,12 @@ randomize() { grep '[^ \t]' | sort -R; }
 echo 'Enter your desired amount of lines generated'
 read lines
 
-a=./words/adjectives
-n=./words/nouns
+a=./words/adjectives.txt
+n=./words/nouns.txt
 
 while (( lines-- )) && read a && read n <&3
 do
     echo "$a $n"
 sleep .5
 done < <(randomize < $a) 3< <(randomize < $n)
+$SHELL
